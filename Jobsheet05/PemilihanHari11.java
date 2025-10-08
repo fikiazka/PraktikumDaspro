8bf02aba700def5f34c05484696d96dc73155538
fikiazka <fikiiazka@gmail.com>
2025-10-08 09:42:07 +0700

    Percobaan 2

---
+++ Jobsheet05/PemilihanHari11.java
@@ -0,0 +1,31 @@
+package PraktikumDaspro.Jobsheet05;
+
+import java.util.Scanner;
+
+public class PemilihanHari11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String dayName, dayType;
+
+        System.out.print("Input day name: ");
+        dayName = sc.nextLine();
+
+        switch (dayName) {
+            case "monday":
+            case "tuesday":
+            case "wednesday":
+            case "friday":
+                dayType = "weekday";
+                break;
+            case "saturday":
+            case "sunday":
+                dayType = "weekend";
+                break;
+            default:
+                dayType = "invalid day name";
+        }
+        System.out.println(dayName + " is a " + dayType);
+        sc.close();
+    }
+}
