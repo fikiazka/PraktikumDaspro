a49015eb11847cc9b148d70995177aaf21454fdf
fikiazka <fikiiazka@gmail.com>
2025-10-11 16:58:10 +0700

    Tugas 1

---
+++ Jobsheet05/Tugas1.java
@@ -0,0 +1,18 @@
+package PraktikumDaspro.Jobsheet05;
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+    }
+}
