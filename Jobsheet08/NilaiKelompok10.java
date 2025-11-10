7eeef10eef5a72783963b8eae49dd77c19235cdf
fikiazka <fikiiazka@gmail.com>
2025-11-10 19:06:01 +0700

    nilai kelompok 10

---
+++ Jobsheet08/NilaiKelompok10.java
@@ -0,0 +1,27 @@
+package PraktikumDaspro.Jobsheet08;
+
+import java.util.Scanner;
+
+public class NilaiKelompok10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, totalNilai, rataNilai;
+        int i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Masukkan nilai ke-" + j + ": ");
+                nilai = sc.nextDouble();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata - rata: " + rataNilai);
+            i++;
+        }
+        sc.close();
+    }
+}
