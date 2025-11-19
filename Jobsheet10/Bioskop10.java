915abb267b3db629862b691c9220a16790551f91
fikiazka <fikiiazka@gmail.com>
2025-11-19 09:39:21 +0700

    add string join

--- Jobsheet10/Bioskop10.java
+++ Jobsheet10/Bioskop10.java
@@ -16,9 +16,8 @@ public class Bioskop10 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        System.out.println("Penonton pada baris ke-3: ");
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
