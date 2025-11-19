8a624d22da1c63b629bfab8aaf2d68cf4cc49d4c
fikiazka <fikiiazka@gmail.com>
2025-11-19 11:19:13 +0700

    add menu bioskop

--- Jobsheet10/BioskoWithScanner10.java
+++ Jobsheet10/BioskoWithScanner10.java
@@ -4,29 +4,54 @@ public class BioskoWithScanner10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int baris, kolom;
-        String nama, next;
         String[][] penonton = new String[4][2];
+        int baris, kolom, menu;
+        String nama, daftarPenonton;
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
-
+        do {
+            System.out.println("==== Menu Bioskop ====");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                    System.out.print("Masukkan baris (1-4): ");
+                    baris = sc.nextInt();
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                    System.out.print("Masukkan kolom(1-2): ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
+
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.println("Data Berhasil Disimpan!");
+                    break;
+                case 2:
+                    System.out.println("==== Daftar Penonton ====");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            daftarPenonton = (penonton[i][j] == null) ? "-" : penonton[i][j];
+                            System.out.print(daftarPenonton + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Program selesai. Terima Kasih!");
+                    break;
+                default:
+                    System.out.println("Input Invalid!");
+                    break;
             }
-        }
+
+        } while (menu != 3);
 
         sc.close();
     }
