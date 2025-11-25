08e4979fa0475be5caa1e45dd94f2d2148a467eb
fikiazka <fikiiazka@gmail.com>
2025-11-25 21:56:10 +0700

    siakad 10

---
+++ Jobsheet10/SIAKAD10.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SIAKAD10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+
+        int[][] nilai = new int[jumlahMahasiswa][jumlahMatkul];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPersiswa = 0;
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPersiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai rata-rata: " + (totalPersiswa / jumlahMatkul));
+        }
+
+        System.out.println("=======================================");
+        System.out.println("Rata - rata Nilai setiap Mata Kuliah:");
+
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < jumlahMahasiswa; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + (totalPerMatkul / jumlahMahasiswa));
+        }
+
+        sc.close();
+    }
+}
