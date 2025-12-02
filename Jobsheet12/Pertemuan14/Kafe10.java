7fe70136f4a43554baa481cc02a738010335c4d4
fikiazka <fikiiazka@gmail.com>
2025-12-02 11:05:12 +0700

    Kafe tanpa parameter

---
+++ Jobsheet12/Pertemuan14/Kafe10.java
@@ -0,0 +1,18 @@
+package Pertemuan14;
+
+public class Kafe10 {
+    public static void main(String[] args) {
+        Menu();
+    }
+    public static void Menu(){
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - Rp 15,0000");
+        System.out.println("2. Cappucino  - Rp 20,0000");
+        System.out.println("3. Latte      - Rp 22,0000");
+        System.out.println("4. Teh Tarik  - Rp 12,0000");
+        System.out.println("5. Roti Bakar - Rp 10,0000");
+        System.out.println("6. Mie Goreng - Rp 18,0000");
+        System.out.println("==========================");
+        System.out.println("Silahkan Pilih menu yang anda inginkan.");
+    }
+}
