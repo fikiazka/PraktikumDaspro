46bf0ce5eb30ac206256690aaf46d1acb0631391
fikiazka <fikiiazka@gmail.com>
2025-12-03 08:33:19 +0700

    pengunjung cafe varargs

---
+++ Jobsheet12/Pertemuan14/PengunjungCafe10.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class PengunjungCafe10 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+    static void daftarPengunjung (String...namaPengunjung){
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+}
