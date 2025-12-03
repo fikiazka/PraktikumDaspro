f5fa355313b9ed6ea9fcec4be5ce2329f21eed60
fikiazka <fikiiazka@gmail.com>
2025-12-03 09:40:27 +0700

    hitung balok

---
+++ Jobsheet12/Pertemuan14/HitungBalok10.java
@@ -0,0 +1,36 @@
+package Pertemuan14;
+
+import java.util.Scanner;
+
+public class HitungBalok10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p, l, t, L, vol;
+
+        System.out.println("Masukkan panjang");
+        p = sc.nextInt();
+
+        System.out.println("Masukkan lebar");
+        l = sc.nextInt();
+
+        System.out.println("Masukkan tinggi");
+        t = sc.nextInt();
+
+        L = p * l;
+        System.out.println("Luas persegi panjang adalah " + L);
+        vol = p * l * t;
+        System.out.println("Volumen balok adalah " + vol);
+
+        sc.close();
+    }
+
+    static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+}
