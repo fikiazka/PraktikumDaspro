3647c6d45504688ec45a4cea8bdbf72ce7fd24c9
fikiazka <fikiiazka@gmail.com>
2025-12-08 20:41:04 +0700

    modifikasi input menu & hari

--- Jobsheet12/Pertemuan14/RekapPenjualanCafe10.java
+++ Jobsheet12/Pertemuan14/RekapPenjualanCafe10.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class RekapPenjualanCafe10 {
 
     static Scanner input = new Scanner(System.in);
-    static String[] daftarMenu = { "Kopi", "Teh", "Es Degan", "Roti Bakar", "Gorengan" };
 
     // Fungsi input data penjualan
-    public static void inputDataPenjualan(int[][] dataPenjualanHarian) {
-        for (int menu = 0; menu < 5; menu++) {
+    public static void inputDataPenjualan(int[][] dataPenjualanHarian, String[] daftarMenu, int jumlahHari,
+            int jumlahMenu) {
+        for (int menu = 0; menu < jumlahMenu; menu++) {
             System.out.println("\nInput penjualan untuk menu: " + daftarMenu[menu]);
-            for (int hari = 0; hari < 7; hari++) {
+            for (int hari = 0; hari < jumlahHari; hari++) {
                 System.out.print("Hari ke-" + (hari + 1) + ": ");
                 dataPenjualanHarian[menu][hari] = input.nextInt();
             }
@@ -19,71 +19,85 @@ public class RekapPenjualanCafe10 {
     }
 
     // Fungsi tampil data penjualan
-    public static void tampilRekapPenjualan(int[][] rekapPenjualan) {
+    public static void tampilRekapPenjualan(int[][] rekapPenjualan, String[] daftarMenu, int jumlahHari,
+            int jumlahMenu) {
         System.out.println("\nREKAP PENJUALAN\n");
 
         System.out.printf("%-15s", "Menu/Hari");
-        for (int hari = 1; hari <= 7; hari++) {
+        for (int hari = 0; hari < jumlahHari; hari++) {
             System.out.printf("%-8s", "H" + hari);
         }
         System.out.println();
 
-        for (int menu = 0; menu < daftarMenu.length; menu++) {
-            System.out.printf("%-15s", daftarMenu[menu]); 
+        for (int menu = 0; menu < jumlahMenu; menu++) {
+            System.out.printf("%-15s", daftarMenu[menu]);
 
-            for (int hari = 0; hari < 7; hari++) {
-                System.out.printf("%-8d", rekapPenjualan[menu][hari]); 
+            for (int hari = 0; hari < jumlahHari; hari++) {
+                System.out.printf("%-8d", rekapPenjualan[menu][hari]);
             }
             System.out.println();
         }
     }
 
     // Fungsi mencari menu terlaris
-    public static void cariMenuTerlaris(int[][] menuTerlaris) {
-        int[] totalPerMenu = new int[5];
+    public static void cariMenuTerlaris(int[][] menuTerlaris, String[] daftarMenu, int jumlahHari, int jumlahMenu) {
+        int[] totalPerMenu = new int[jumlahMenu];
 
-        for (int menu = 0; menu < 5; menu++) {
-            for (int hari = 0; hari < 7; hari++) {
+        for (int menu = 0; menu < jumlahMenu; menu++) {
+            for (int hari = 0; hari < jumlahHari; hari++) {
                 totalPerMenu[menu] += menuTerlaris[menu][hari];
             }
         }
 
-        int nilaiTertinggi = totalPerMenu[0];
-        int indexMenuTerlaris = 0;
+        int indexTerlaris = 0;
 
-        for (int i = 1; i < 5; i++) {
-            if (totalPerMenu[i] > nilaiTertinggi) {
-                nilaiTertinggi = totalPerMenu[i];
-                indexMenuTerlaris = i;
+        for (int i = 1; i < jumlahMenu; i++) {
+            if (totalPerMenu[i] > totalPerMenu[indexTerlaris]) {
+                indexTerlaris = i;
             }
         }
-
         System.out.println("\nMenu dengan penjualan tertinggi:");
-        System.out.println(daftarMenu[indexMenuTerlaris] + " = " + nilaiTertinggi + " porsi");
+        System.out.println(daftarMenu[indexTerlaris] + " = " + totalPerMenu[indexTerlaris] + " porsi");
     }
 
     // Fungsi hitung rata-rata
-    public static void hitungRataPenjualan(int[][] rataPenjualan) {
+    public static void hitungRataPenjualan(int[][] rataPenjualan, String[] daftarMenu, int jumlahHari, int jumlahMenu) {
         System.out.println("\nRata-rata penjualan tiap menu selama 7 hari):");
 
-        for (int menu = 0; menu < 5; menu++) {
+        for (int menu = 0; menu < jumlahMenu; menu++) {
             int total = 0;
 
-            for (int hari = 0; hari < 7; hari++) {
+            for (int hari = 0; hari < jumlahHari; hari++) {
                 total += rataPenjualan[menu][hari];
             }
 
-            double rata = total / 7.0;
+            double rata = total * 1.0 / jumlahHari;
             System.out.printf("%s = %.2f%n", daftarMenu[menu], rata);
         }
     }
 
     public static void main(String[] args) {
-        int[][] dataPenjualanMenu = new int[5][7];
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = input.nextInt();
+        input.nextLine(); // buang newline
 
-        inputDataPenjualan(dataPenjualanMenu);
-        tampilRekapPenjualan(dataPenjualanMenu);
-        cariMenuTerlaris(dataPenjualanMenu);
-        hitungRataPenjualan(dataPenjualanMenu);
+        String[] daftarMenu = new String[jumlahMenu];
+
+        System.out.println("\nMasukkan nama setiap menu:");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Menu " + (i + 1) + ": ");
+            daftarMenu[i] = input.nextLine();
+        }
+
+        System.out.print("\nMasukkan jumlah hari penjualan: ");
+        int jumlahHari = input.nextInt();
+
+        int[][] dataPenjualan = new int[jumlahMenu][jumlahHari];
+
+        // Pemanggilan fungsi
+        inputDataPenjualan(dataPenjualan, daftarMenu, jumlahHari, jumlahMenu);
+        tampilRekapPenjualan(dataPenjualan, daftarMenu, jumlahHari, jumlahMenu);
+        cariMenuTerlaris(dataPenjualan, daftarMenu, jumlahHari, jumlahMenu);
+        hitungRataPenjualan(dataPenjualan, daftarMenu, jumlahHari, jumlahMenu);
     }
 }
